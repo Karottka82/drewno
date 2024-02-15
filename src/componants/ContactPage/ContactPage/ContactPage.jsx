884bff075a1546a268efaec444e6e8e490cacf04
Jan Derabin <karolinaderabin@gmail.com>
2024-02-15 12:32:25 +0100

    po prawej

--- src/componants/ContactPage/ContactPage/ContactPage.jsx
+++ src/componants/ContactPage/ContactPage/ContactPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaPhone, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa";
 import "./ContactPage.css";
 import { TopBar } from "../../TopBar/TopBar.jsx";
 import { FirstSectionContact } from "../1/FirstSectionContact.jsx";
@@ -9,20 +8,6 @@ export const ContactPage = () => {
     <section id="/contact">
       <TopBar />
       <FirstSectionContact />
-      <div className="contact-container">
-        <h1> </h1>
-        <ul className="contact-list">
-          <p>
-            <FaPhone /> 888-133-916 lub 517-946-548
-          </p>
-          <p>
-            <FaMapMarkerAlt /> Kraków i okopce
-          </p>
-          <p>
-            <FaEnvelope /> drewnorenova@gmail.com
-          </p>
-        </ul>
-      </div>
     </section>
   );
 };
--- src/componants/TopBar/TopBar.jsx
+++ src/componants/TopBar/TopBar.jsx
@@ -7,53 +7,13 @@ export const TopBar = () => {
 
   return (
     <>
-      <br />
-      <Navbar
-        style={{
-          backgroundColor: "#1f100d",
-          marginTop: "10px",
-          justifyContent: "flex-end",
-        }}
-      >
-        <Container className="containerdstyle justify-end">
-          <Navbar.Brand
-            href="/"
-            className="NavbarBrand-custom"
-            style={{
-              color: "#f2d8c7",
-              fontSize: "18px",
-              marginTop: "20px",
-            }}
-          >
-            STRONA GŁOWNA
-          </Navbar.Brand>
-          <Nav
-            ref={navRef}
-            className="me-auto"
-            style={{
-              color: "#f2d8c7",
-              fontSize: "16px",
-              marginTop: "20px",
-            }}
-          >
-            <Nav.Link
-              href="/about"
-              style={{ color: "#f2d8c7", fontSize: "16px", marginLeft: "10px" }}
-            >
-              O NAS
-            </Nav.Link>
-            <Nav.Link
-              href="/services"
-              style={{ color: "#f2d8c7", fontSize: "16px", marginLeft: "10px" }}
-            >
-              USŁUGI
-            </Nav.Link>
-            <Nav.Link
-              href="/contact"
-              style={{ color: "#f2d8c7", fontSize: "16px", marginLeft: "10px" }}
-            >
-              KONTAKT
-            </Nav.Link>
+      <Navbar style={{ backgroundColor: "#1f100d" }}>
+        <Container style={{ justifyContent: "flex-end" }}>
+          <Nav>
+            <Navbar.Brand href="/">STRONA GŁOWNA</Navbar.Brand>
+            <Nav.Link href="/about">O NAS</Nav.Link>
+            <Nav.Link href="/services">USŁUGI</Nav.Link>
+            <Nav.Link href="/contact">KONTAKT</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
